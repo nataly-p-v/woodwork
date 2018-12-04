779e536d05e547bb8bbbf5c3fb637616fa3edee5
Nataliia_Pachkovska <Nataliia_Pachkovska@epam.com>
2018-12-04 12:15:48 +0200

    contacts rus

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ class App extends Component {
           <Layout>
               <Header title="Title" scroll>
                   <Navigation>
-                      <Link to="/">Home</Link>
-                      <Link to="/about">About</Link>
-                      <Link to="/contacts">Contacts</Link>
-                      <Link to="/gallery">Gallery</Link>
-                      <Link to="/prices">Prices</Link>
-                      <Link to="/products">Products</Link>
+                      <Link to="/">Главная</Link>
+                      <Link to="/about">О нас</Link>
+                      <Link to="/contacts">Контакты</Link>
+                      <Link to="/gallery">Галерея</Link>
+                      <Link to="/prices">Цены</Link>
+                      <Link to="/products">Продукция</Link>
                   </Navigation>
               </Header>
               <Drawer title="Title">
                   <Navigation>
-                      <Link to="/">Home</Link>
-                      <Link to="/about">About</Link>
-                      <Link to="/contacts">Contacts</Link>
-                      <Link to="/gallery">Gallery</Link>
-                      <Link to="/prices">Prices</Link>
-                      <Link to="/products">Products</Link>
+                      <Link to="/">Главная</Link>
+                      <Link to="/about">О нас</Link>
+                      <Link to="/contacts">Контакты</Link>
+                      <Link to="/gallery">Галерея</Link>
+                      <Link to="/prices">Цены</Link>
+                      <Link to="/products">Продукция</Link>
                   </Navigation>
               </Drawer>
               <Content>
@@ -51,8 +51,8 @@ class App extends Component {
                   </FooterSection>
                   <FooterSection type="bottom" logo="Title">
                       <FooterLinkList>
-                          <Link to="/help">Help</Link>
-                          <Link to="/privacy">Privacy & Terms</Link>
+                          <Link to="/help">Помощь</Link>
+                          <Link to="/privacy">Конфиденциальность и условия</Link>
                       </FooterLinkList>
                   </FooterSection>
               </Footer>
